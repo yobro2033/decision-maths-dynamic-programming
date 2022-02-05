669ca9aad38a0b90afb313cb979b2c384a97f15b
Quoc Viet Phan <80685605+yobro2033@users.noreply.github.com>
2022-02-05 08:02:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ additionalCost = int(input("Please enter your additional cost: "))
 overheadCost = int(input("Please enter your overhead cost: "))
 storageCost = int(input("Please enter your storage cost: "))
 aboveAdditional = int(input("Above which value to require additional cost? "))
-
 availableMax = int(input("Please enter maxium 'items' available: "))
 toMakeMax = int(input("Please enter maxium 'items' can be made: "))
 
@@ -10,9 +9,14 @@ inStock = 0
 toMake = 0
 leftOver = 0
 value = 0
-
 count = 0
+tempRequired = 0
+varCount = 0
+
+valueData = []
 requestOrder = []
+
+
 while True:
     try:
         item = int(input("Enter number of item demanded (leave blank and enter to stop): "))
@@ -21,12 +25,8 @@ while True:
     except ValueError:
         break
 
-tempRequired = 0
-
-valueData = []
-
-varCount = 0
 firstRun = True
+
 for i in range(count):
     tempRequired = requestOrder[count - 1 - i]
     if firstRun == True:
@@ -43,17 +43,38 @@ for i in range(count):
                     elif toMake == 0:
                         value = (inStock*storageCost)
                         valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
-                    elif toMake > 0 and toMake > aboveAdditional:
+                    elif toMake >= 0 and toMake > aboveAdditional:
                         value = (inStock*storageCost) + overheadCost + additionalCost
                         valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
         firstRun = False
+
     else:
         varCount = varCount + 1
         lastRun = False
         if lastRun == False:
             if i == count - 1:
+                for valueD in valueData:
+                    inStock = 0
+                    for k in range(availableMax + 1):
+                        leftOver = k
+                        toMake = int(tempRequired + leftOver - inStock)
+                        if toMake > -1 and toMake <= toMakeMax:
+                            leftOver = inStock + toMake - tempRequired
+                            if leftOver >= 0 and leftOver <= availableMax:
+                                if toMake <= aboveAdditional and toMake > 0:
+                                    if valueD['stage'] == varCount - 1 and valueD['state'] == leftOver:
+                                        value = (inStock*storageCost) + overheadCost + valueD['value']
+                                        valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
+                                elif toMake == 0:
+                                    if valueD['stage'] == varCount - 1 and valueD['state'] == leftOver:
+                                        value = (inStock*storageCost) + valueD['value']
+                                        valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
+                                elif toMake >= 0 and toMake > aboveAdditional:
+                                    if valueD['stage'] == varCount - 1 and valueD['state'] == leftOver:
+                                        value = (inStock*storageCost) + overheadCost + additionalCost +valueD['value']
+                                        valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
                 lastRun = True
-                break
+                
             else:
                 for valueD in valueData:
                     for j in range(availableMax + 1):
@@ -72,7 +93,7 @@ for i in range(count):
                                         if valueD['stage'] == varCount - 1 and valueD['state'] == leftOver:
                                             value = (inStock*storageCost) + valueD['value']
                                             valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
-                                    elif toMake > 0 and toMake > aboveAdditional:
+                                    elif toMake >= 0 and toMake > aboveAdditional:
                                         if valueD['stage'] == varCount - 1 and valueD['state'] == leftOver:
                                             value = (inStock*storageCost) + overheadCost + additionalCost +valueD['value']
                                             valueData.append({'stage': varCount, 'state': inStock, 'action': toMake, 'destination': leftOver, 'value': value})
